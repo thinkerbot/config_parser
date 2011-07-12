c4827b93f26ac6d3ac069dd5cb08ecda0bcff7bd
Simon Chiang <simon.a.chiang@gmail.com>
2011-07-11 21:16:07 -0600

    reordered some readme sections for clarity

--- test/readme_test.rb
+++ test/readme_test.rb
@@ -4,6 +4,25 @@ require 'config_parser'
 class ReadmeTest < Test::Unit::TestCase
   def test_documentation
     parser = ConfigParser.new
+    parser.add :option, 'default'      # regular option with a default value
+    parser.add :switch, true           # true makes a --[no-]switch
+    parser.add :flag, false            # false as a default makes a --flag
+    parser.add :list, []               # an array makes a list-style option
+
+    expected = ['a', 'b', 'c']
+    assert_equal expected, parser.parse('a b --flag --list x --list y,z c')
+
+    expected = {
+      :option => 'default',
+      :switch => true,
+      :flag   => true,
+      :list   => ['x', 'y', 'z']
+    }
+    assert_equal expected, parser.config
+
+    #
+
+    parser = ConfigParser.new
     parser.on '--option OPTION', 'a standard option' do |value|
       parser[:option] = value
     end
@@ -26,32 +45,6 @@ class ReadmeTest < Test::Unit::TestCase
     }
     assert_equal expected, parser.config
 
-    expected = %q{
-        --option OPTION              a standard option
-        --[no-]switch                a switch
-        --flag                       a flag
-}
-    assert_equal expected, "\n" + parser.to_s
-
-    #
-
-    parser = ConfigParser.new
-    parser.add :option, 'default'      # regular option with a default value
-    parser.add :switch, true           # true makes a --[no-]switch
-    parser.add :flag, false            # false as a default makes a --flag
-    parser.add :list, []               # an array makes a list-style option
-
-    expected = ['a', 'b', 'c']
-    assert_equal expected, parser.parse('a b --flag --list x --list y,z c')
-
-    expected = {
-      :option => 'default',
-      :switch => true,
-      :flag   => true,
-      :list   => ['x', 'y', 'z']
-    }
-    assert_equal expected, parser.config
-    
     #
 
     parser = ConfigParser.new
